8a15a974a5f4825917cb4433a375228cc1e627f9
rohit.p <rohit.p@collabera.com>
2022-01-04 12:26:34 +0530

    first commit

---
+++ week-1/day-1/src/test/java/AdditionTest.java
@@ -0,0 +1,19 @@
+public class AdditionTest {
+    public void testAdditionCase (int num1 , int num2)
+    {
+        int age=10;
+        double sal=20.3;
+        char c='a';
+        long ln=1234567L;
+        short s=124;
+        String str="new java";
+        boolean mgr= false;
+        float  g = 20.4f;
+    }
+    public void methods()
+    {
+
+    }
+
+
+}
