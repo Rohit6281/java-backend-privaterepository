822cdacebce7a63f23457c53d8e9eaa5111749d4
rohit.p <rohit.p@collabera.com>
2022-01-06 19:35:46 +0530

    parallel programing : thread

--- week-1/parallel-programing/src/main/java/main.java
+++ week-1/parallel-programing/src/main/java/main.java
@@ -1,12 +1,13 @@
 import com.sun.tools.javac.Main;
 
 import java.util.ArrayList;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class main {
     public  void withStream(){
-        IntStream.of(1,2,4,6,7,8).parallel().filter(el-> el>10).forEach(el-> System.out.println(el));
+        IntStream.of(1,2,4,6,7,8).parallel().filter(el-> el>2).forEach(el-> System.out.println(el));
         Stream.of("abc","pqr","lmn");
     }
     public void withoutStream(){
@@ -19,9 +20,34 @@ public class main {
             System.out.println(num*num);
         }
     }
+    public void futures(){
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        Future<String> task1=service.submit(()->{
+            Thread.sleep(2500);
+            return "hello world from task1";
+        });
+        Future<String> task2=service.submit(()->{
+            Thread.sleep(5500);
+            return "hello world from task2";
+
+        });
+        try{
+            if(!task1.isCancelled()){
+                System.out.println("task 1 completed  : "+task1.get());
+            }
+            if(!task2.isCancelled()){
+                System.out.println("task2 completed :"+task2.get());
+            }
+
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
 
     public static void main(String[] args) {
         main m =new main();
-        m.withoutStream();
+        m.futures();
     }
 }
