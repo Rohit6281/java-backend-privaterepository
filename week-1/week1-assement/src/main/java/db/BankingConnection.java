e9f96c468f14120e21460fceca5f4dee81e3f445
rohit.p <rohit.p@collabera.com>
2022-01-09 16:04:51 +0530

    week-1-assesement

--- week-1/week1-assement/src/main/java/db/BankingConnection.java
+++ week-1/week1-assement/src/main/java/db/BankingConnection.java
@@ -9,9 +9,9 @@ import static java.lang.Class.forName;
 public class BankingConnection {
     private final Connection connection;
 
-    public BankingConnection()throws ClassNotFoundException, SQLException{
+    public BankingConnection() throws ClassNotFoundException, SQLException {
         forName("com.mysql.cj.jdbc.Driver");
-        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/Bnk_acc", "root", "password");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bnk_acc", "root", "password");
         connection.setAutoCommit(false);
         System.out.println("connected to mysql");
     }
