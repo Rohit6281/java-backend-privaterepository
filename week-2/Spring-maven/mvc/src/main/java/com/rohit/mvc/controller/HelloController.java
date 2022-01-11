5bb79ad09effcc5b583f9c5486f8d33eb5a153a1
rohit.p <rohit.p@collabera.com>
2022-01-11 13:32:30 +0530

    mvc login

---
+++ week-2/Spring-maven/mvc/src/main/java/com/rohit/mvc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.rohit.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/login")
+    public String login() {
+        // after doing processing here, view will be returned
+        return "login";
+    }
+}
