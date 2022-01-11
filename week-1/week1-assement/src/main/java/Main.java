a189a7e7153217a3490636b5eed424dfc5aa724b
rohit.p <rohit.p@collabera.com>
2022-01-11 17:59:33 +0530

    week-1-assesement

--- week-1/week1-assement/src/main/java/Main.java
+++ week-1/week1-assement/src/main/java/Main.java
@@ -8,32 +8,32 @@ import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
-    public class Main {
-        public static void main(String[] args) throws SQLException, ClassNotFoundException {
-            BankingConnection connection = new BankingConnection();
-            BnkService service = new BnkService(connection.getConnection());
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        BankingConnection connection = new BankingConnection();
+        BnkService service = new BnkService(connection.getConnection());
 
-            Menu m=new Menu();
-            m.showMenu();
-            //    int cnt=service.createBnkAcc(1,500,"rohit", Date.valueOf(LocalDate.now()),true);
-            //  int cnt1 =service.createBnkAcc(2,10000,"rn",Date.valueOf(LocalDate.now()),true);
-            //  service.createBnkAcc(3,28000,"sohan",Date.valueOf(LocalDate.now()),true);
+        Menu m = new Menu();
+        m.showMenu();
+        //    int cnt=service.createBnkAcc(1,500,"rohit", Date.valueOf(LocalDate.now()),true);
+        //  int cnt1 =service.createBnkAcc(2,10000,"rn",Date.valueOf(LocalDate.now()),true);
+        //  service.createBnkAcc(3,28000,"sohan",Date.valueOf(LocalDate.now()),true);
 //       if(cnt1>0){
 //           System.out.println("Bnk created suceessfully");
 //       }
-            //   service.showAllAcc();
-            //  service.checkBalance("rohit");
-            //service.checkBalance("rn");
-            // service.transferMoney("rn");
-            //   service.transferMoney("rn",490);
-            //   service.transferMoney("rohit",300);
-            // service.deposit(100,2);
-            //  service.withdraMoney("rohit");
-            //service.Deactivate("rn",false);
-            service.reActivate("rn", true);
-
-
-        }
+        //   service.showAllAcc();
+        //  service.checkBalance("rohit");
+        //service.checkBalance("rn");
+        // service.transferMoney("rn");
+        //   service.transferMoney("rn",490);
+        //   service.transferMoney("rohit",300);
+        // service.deposit(100,2);
+        //  service.withdraMoney("rohit");
+        //service.Deactivate("rn",false);
+        service.reActivate("rn", true);
 
 
     }
+
+
+}
--- week-1/week1-assement/src/main/java/Menu/Menu.java
+++ week-1/week1-assement/src/main/java/Menu/Menu.java
@@ -7,18 +7,27 @@ public class Menu {
         String title = "\n --- Banking System ---";
         String m1 = "\n 1. Create New Account";
         String m2 = "\n 2. Show All Accounts";
-        String m3 = "\n 3. Exit";
+        String m3 = "\n 3.Display balance ";
+        String m4 = "\n 4. transfer money";
+        String m5 = "\n 5. withdraw";
+        String m6 = "\n 6. deposit";
+        String m7 = "\n 7.activate acc";
+        String m8 = "\n 8.Deactivate acc";
+        String m9 = "\n 9. Exit";
 
-        return title + m1 + m2 + m3;
+        return title + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9;
     }
 
     public void showMenu() {
         var scanner = new Scanner(System.in);
+
         while (true) {
             System.out.println(prepareMenu());
+            System.out.println(" *) enter the details : ");
             int ch = scanner.nextInt();
 
-            if (ch == 3) System.exit(1);
+
+            if (ch == 9) System.exit(1);
             if (ch == 1) {
                 System.out.println("Enter Account Number : ");
                 int ac_Num = scanner.nextInt();
@@ -29,6 +38,25 @@ public class Menu {
                 System.out.println("Enter Name : ");
                 String ac_hl_nm = scanner.nextLine();
 
+
+
+            }
+            if(ch==2){
+                System.out.println("Enter the acc_hl_nm");
+                String ac_hl_nm=scanner.nextLine();
+              //  service.showAllacc(ac_hl_nm);
+            }
+            if(ch==3){
+                System.out.println("enter the ac_hl_nm");
+                int amount=scanner.nextInt();
+            }
+            if(ch==4){
+                System.out.println("enter the transfer acc num");
+                int acc_num=scanner.nextInt();
+            }
+            if(ch==5){
+                System.out.println("withdraw");
+                int money=scanner.nextInt();
             }
         }
     }
