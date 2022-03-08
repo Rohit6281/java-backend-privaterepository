db2dc39dd2ab5b489bf9ff3295786dddda2ad8be
rohit.p <rohit.p@collabera.com>
2022-03-08 13:01:58 +0530

    commit : final test cases

--- final-case-study/hospital-billing-module/src/test/java/com/rohit/project/billing/hospitalbillingmodule/billDto/BillDtoTests.java
+++ final-case-study/hospital-billing-module/src/test/java/com/rohit/project/billing/hospitalbillingmodule/billDto/BillDtoTests.java
@@ -1,5 +1,6 @@
 package com.rohit.project.billing.hospitalbillingmodule.billDto;
 
+import com.rohit.project.billing.hospitalbillingmodule.domain.Bill;
 import com.rohit.project.billing.hospitalbillingmodule.dto.BillDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,5 +19,25 @@ public class BillDtoTests {
     void testBillDtoValidObject() {
         BillDto bill = new BillDto();
         Assertions.assertNotNull(bill);
+    }
+    @DisplayName("Domain : Checking getters and setters")
+    @Test
+    void testObjectGettersSetters() {
+        var abc = new Bill();
+        abc.setId(3);
+        abc.setPatientName("reddy");
+        abc.setTreatment("covid");
+        abc.setAmount( 20000.00);
+        abc.setStatus(true);
+        abc.getPatientName();
+
+        Assertions.assertEquals(3, abc.getId());
+        Assertions.assertEquals("reddy" , abc.getPatientName());
+        Assertions.assertEquals(20000.00,abc.getAmount());
+        Assertions.assertEquals(true,abc.getStatus());
+        Assertions.assertEquals(
+                ("covid")
+                , abc.getTreatment()
+        );
     }
 }
